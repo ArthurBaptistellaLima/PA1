03dcbd5c8e765f21f3ac9b675f8b54645f766c91
ArthurBaptistella <bapp01w@gmail.com>
2026-04-15 12:28:47 -0300

    Add files via upload

---
+++ Projetos/Exercicio 2/src/exercicio/pkg2/Exercicio2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio.pkg2;
+
+/**
+ *
+ * @author Admin
+ */
+public class Exercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int V1=0;
+        double V2=0;
+        int V3=0;
+        V1=10;
+        V2=V1+5.6;
+        V3=6;
+        System.out.println("Variavel 1 e "+ V1);
+        System.out.println("Variavel 2 e "+ V2);
+        System.out.println("Variavel 3 e "+ V3);
+        // TODO code application logic here
+    }
+    
+}
---
+++ Projetos/Exercicio 3/src/exercicio/pkg3/Exercicio3.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio.pkg3;
+
+/**
+ *
+ * @author Admin
+ */
+public class Exercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int V1=100;
+        double V2=0;
+        V2=V1;
+        System.out.println("O Valor da Segunda Variavel e "+ V2);
+        // TODO code application logic here
+    }
+    
+}
